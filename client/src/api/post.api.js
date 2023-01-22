28aae447d45e67d34fe359a84316c401c7d94227
Marwen-Mrabti-chantier <m.mrabti@chantier.tn>
2023-01-22 02:04:26 +0100

    dalle clone

--- client/src/api/post.api.js
+++ client/src/api/post.api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const API_URL = "http://localhost:8081/api/v1";
+//const API_URL = "http://localhost:8081/api/v1"; //local
+const API_URL = "https://dalle-2clone.onrender.com/api/v1"; //local
 
 export const createPhoto = async (prompt) => {
   const { data } = await axios.get(`${API_URL}/dalle?prompt=${prompt}`);
